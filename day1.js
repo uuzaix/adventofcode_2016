3cb367970cfbaebbf98cc37ee3025b83cd37d0fd
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-12-02 20:56:09 +0100

    Read input form stdin

--- day1.js
+++ day1.js
@@ -1,6 +1,15 @@
-const input = 'L5, R1, L5, L1, R5, R1, R1, L4, L1, L3, R2, R4, L4, L1, L1, R2, R4, R3, L1, R4, L4, L5, L4, R4, L5, R1, R5, L2, R1, R3, L2, L4, L4, R1, L192, R5, R1, R4, L5, L4, R5, L1, L1, R48, R5, R5, L2, R4, R4, R1, R3, L1, L4, L5, R1, L4, L2, L5, R5, L2, R74, R4, L1, R188, R5, L4, L2, R5, R2, L4, R4, R3, R3, R2, R1, L3, L2, L5, L5, L2, L1, R1, R5, R4, L3, R5, L1, L3, R4, L1, L3, L2, R1, R3, R2, R5, L3, L1, L1, R5, L4, L5, R5, R2, L5, R2, L1, L5, L3, L5, L5, L1, R1, L4, L3, L1, R2, R5, L1, L3, R4, R5, L4, L1, R5, L1, R5, R5, R5, R2, R1, R2, L5, L5, L5, R4, L5, L4, L4, R5, L2, R1, R5, L1, L5, R4, L3, R4, L2, R3, R3, R3, L2, L2, L2, L1, L4, R3, L4, L2, R2, R5, L1, R2';
+const readline = require('readline');
 
-const input_list = input.split(", ");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', (data) => {
+  const input_list = data.split(", ");
+  console.log(find_distance(input_list));
+});
+
 
 function find_distance(input) {
   let dir = 0;
@@ -34,7 +43,5 @@ function find_distance(input) {
         break;
     }
   }
-  return (Math.abs(x) + Math.abs(y));
+  return (Math.abs(x + y));
 }
-
-console.log(find_distance(input_list));
