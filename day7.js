cef634fdccc394003d4fca246cfc1ba7caf67e2f
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-12-10 00:28:58 +0100

    Add day7.1 solution

--- day7.js
+++ day7.js
@@ -5,8 +5,8 @@ function format(line) {
 }
 
 function hasRepetition(line) {
-  const re = line.search(/(.)(.)\2\1/);
-  if (re !== - 1) {
+  const rep = line.match(/(.)(.)\2\1/);
+  if (rep !== null && rep[1] !== rep[2]) {
     return true;
   } else {
     return false;
@@ -17,25 +17,23 @@ function checkData(data) {
   let result = 0;
   data.forEach(array => {
     let countAbba = 0, countAnti = 0;
-    for (var i = 0; i < array.length; i++) {
-      if (i % 2 !== 0 || i === 0) {
-        if (hasRepetition) {
+    array.forEach((str, i) => {
+      if (i % 2 === 0 || i === 0) {
+        if (hasRepetition(str)) {
           countAbba += 1;
         }
       }
-      if (i % 2 === 0) {
-        if (hasRepetition) {
+      if (i % 2 !== 0) {
+        if (hasRepetition(str)) {
           countAnti += 1;
         }
       }
-    }
-    if (countAbba > 0, countAnti === 0) {
+    })
+    if (countAbba > 0 && countAnti === 0) {
       result += 1;
     }
   })
   console.log(result);
 };
-
-// console.log(hasRepetition("abaasdffg"))
 
 readInput(format, checkData);
