0721be1d9361b1cde4dccca3a7011ed734a8738e
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-12-12 00:51:27 +0100

    Add day7.2 solution

--- day7.js
+++ day7.js
@@ -1,17 +1,8 @@
 const _ = require('underscore');
-const { readInput } = require('./helper_funcions.js')
+const { readInput, broadcast } = require('./helper_funcions.js')
 
 function format(line) {
   return line.split(/\[|\]/);
-}
-
-function hasAbba(line) {
-  const rep = line.match(/(.)(.)\2\1/);
-  if (rep !== null && rep[1] !== rep[2]) {
-    return true;
-  } else {
-    return false;
-  }
 }
 
 function extractEven(array) {
@@ -26,15 +17,55 @@ function extractOdd(array) {
   });
 }
 
+function hasAbba(line) {
+  const rep = line.match(/(.)(.)\2\1/);
+  if (rep !== null && rep[1] !== rep[2]) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function supportsTLS(array) {
   if (_.any(extractEven(array).map(hasAbba)) && !_.any(extractOdd(array).map(hasAbba))) {
     return true
   }
 }
 
+function findAbaInStr(line) {
+  let abas = [];
+  for (var i = 0; i < line.length - 2; i++) {
+    let part = line.slice(i, i + 3);
+    if (part[0] === part[2] && part[0] !== part[1]) {
+      abas.push(part);
+    }
+  }
+  return abas;
+}
+
+function getAbas(array) {
+  return array.map(findAbaInStr).reduce((a, b) => {
+    return a.concat(b)
+  });
+}
+
+function supportsSSL(array) {
+  const abas = getAbas(extractOdd(array));
+  const babs = getAbas(extractEven(array));
+  for (a of abas) {
+    if (babs.includes(a[1] + a[0] + a[1])) {
+      return true;
+    }
+  }
+  return false;
+}
 
 function checkData1(data) {
-  console.log(data.filter(supportsTLS).length);
+  console.log('Part1: ' + data.filter(supportsTLS).length);
 };
 
-readInput(format, checkData1);
+function checkData2(data) {
+  console.log('Part1: ' + data.filter(supportsSSL).length);
+};
+
+readInput(format, broadcast(checkData1, checkData2));
