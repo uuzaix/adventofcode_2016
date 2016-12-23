3034b2f170037f7010fa5f31e28c91ccd6d6885b
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-12-24 00:03:46 +0100

    Update day22.1

--- day22.js
+++ day22.js
@@ -1,20 +1,18 @@
+const _ = require('lodash');
 const { readInput } = require('./helper_funcions.js');
 
 function format(line) {
-  const [, x, y, size, used, aval] = line.match(/-x([0-9]+)-y([0-9]+)\s+([0-9]+)T\s+([0-9]+)T\s+([0-9]+)/)
-  return ({ x: parseInt(x), y: parseInt(y), size: parseInt(size), used: parseInt(used), aval: parseInt(aval) });
+  const [, x, y, size, used, aval] = line.match(/-x([0-9]+)-y([0-9]+)\s+([0-9]+)T\s+([0-9]+)T\s+([0-9]+)/).map(val => parseInt(val))
+  return ({ x, y, size, used, aval });
 }
 
 function day22(data) {
-  let counter = 0;
-  data.forEach((line, index) => {
-    data.forEach((l, ind) => {
-      if (index !== ind && line.used !== 0 && line.used <= l.aval) {
-          counter += 1;
-      }
+  const result = data.map((line, index) => {
+    return data.filter((l, ind) => {
+      return  (index !== ind && line.used !== 0 && line.used <= l.aval)
     });
   });
-  console.log(counter);
+  console.log(_.flatten(result).length);
 }
 
 readInput(format, day22);
