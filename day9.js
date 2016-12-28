b7ae03c60b7c715bde077a0978b3526e20c9ff83
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-12-28 21:38:23 +0100

    Add day9.2

--- day9.js
+++ day9.js
@@ -5,20 +5,30 @@ function format(line) {
   return line.replace('\s', '');
 }
 
-function day9(data) {
-  let input = data[0];
+function decompress(input, part2) {
   const re = /[(](\d+)x(\d+)[)]/;
   let result = 0;
   while (input.length > 0) {
     const match = re.exec(input);
     if (match) {
-      result += match.index + match[1] * match[2];
-      input = input.slice(match.index + match[0].length + match[1], input.length);
+      if (part2) {
+        result += match.index;
+        result += decompress(input.slice(match.index + match[0].length, match.index + match[0].length + parseInt(match[1])), true) * parseInt(match[2])
+      } else {
+        result += match.index + match[1] * match[2];
+      }
+      input = input.slice(match.index + match[0].length + parseInt(match[1]), input.length);
     } else {
       result += input.length;
       input = '';
     }
   }
-  console.log(result);
+  return result;
+}
+
+function day9(data) {
+  let input = data[0];
+  console.log('Part1: ', decompress(input, false));
+  console.log('Part2: ', decompress(input, true));
 }
 readInput(format, day9);
