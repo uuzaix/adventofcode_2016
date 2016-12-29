e27a879bda5d8eb99fd18afe06c33eb51227e46c
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-12-30 00:02:02 +0100

    Add day15.2

--- day15.js
+++ day15.js
@@ -1,22 +1,28 @@
-const { readInput, broadcast } = require('./helper_funcions.js')
+const { readInput } = require('./helper_funcions.js')
 
 function format(line) {
   const [, num, size, pos] = line.match(/#(\d+) has (\d+) .+ (\d+)/).map(val => parseInt(val));
   return { num, size, pos };
 }
 
-function day15(data) {
+function findTime(data) {
   let found = false, time = 0;
   while (!found) {
     if (data.filter(disc => {
-      return (time + disc.num + disc.pos) % disc.size !== 0
+      return (time + disc.num + disc.pos) % disc.size !== 0;
     }).length === 0) {
       found = true;
     } else {
       time++;
     }
   }
-  console.log(time)
+  return time;
+}
+
+function day15(data) {
+  console.log('Part1: ', findTime(data));
+  data.push({ num: 7, size: 11, pos: 0 });
+  console.log('Part2: ', findTime(data));
 }
 
 readInput(format, day15);
